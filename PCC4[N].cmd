2c62df491e79320f609ef6bb12f747684edec923
thomad <thomad@firefly.cit.act.edu.au>
2010-01-15 09:39:24 +1100

    First prototype PCB layout

--- PCC4[N].cmd
+++ PCC4[N].cmd
@@ -10,103 +10,92 @@ ChangePinName(Q1, B, B)
 ChangePinName(Q1, E, E)
 ChangePinName(Q1, C, C)
 
-# Start of element Q11
-ChangePinName(Q11, B, B)
-ChangePinName(Q11, E, E)
-ChangePinName(Q11, C, C)
+# Start of element Q12
+ChangePinName(Q12, B, B)
+ChangePinName(Q12, E, E)
+ChangePinName(Q12, C, C)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
 
 # Start of element R12
 ChangePinName(R12, 1, 1)
 ChangePinName(R12, 2, 2)
 
-# Start of element R11
-ChangePinName(R11, 1, 1)
-ChangePinName(R11, 2, 2)
-
-# Start of element R10
-ChangePinName(R10, 1, 1)
-ChangePinName(R10, 2, 2)
-
-# Start of element R9
-ChangePinName(R9, 1, 1)
-ChangePinName(R9, 2, 2)
-
-# Start of element J10
-ChangePinName(J10, 1, 1)
-ChangePinName(J10, 2, 2)
+# Start of element Q11
+ChangePinName(Q11, B, B)
+ChangePinName(Q11, E, E)
+ChangePinName(Q11, C, C)
 
 # Start of element Q10
 ChangePinName(Q10, B, B)
 ChangePinName(Q10, E, E)
 ChangePinName(Q10, C, C)
 
-# Start of element R6
-ChangePinName(R6, 1, 1)
-ChangePinName(R6, 2, 2)
-
-# Start of element J9
-ChangePinName(J9, 1, 1)
-ChangePinName(J9, 2, 2)
-
-# Start of element R4
-ChangePinName(R4, 1, 1)
-ChangePinName(R4, 2, 2)
-
-# Start of element P9
-ChangePinName(P9, pin, pin)
-
-# Start of element P8
-ChangePinName(P8, pin, pin)
-
-# Start of element P2
-ChangePinName(P2, pin, pin)
-
-# Start of element J4
-ChangePinName(J4, 1, 1)
-ChangePinName(J4, 2, 2)
-
-# Start of element J8
-ChangePinName(J8, 1, 1)
-ChangePinName(J8, 2, 2)
-
-# Start of element R3
-ChangePinName(R3, 1, 1)
-ChangePinName(R3, 2, 2)
+# Start of element J11
+ChangePinName(J11, 1, 1)
+ChangePinName(J11, 2, 2)
 
 # Start of element R2
 ChangePinName(R2, 1, 1)
 ChangePinName(R2, 2, 2)
 
-# Start of element P6
-ChangePinName(P6, pin, pin)
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
 
-# Start of element P5
-ChangePinName(P5, pin, pin)
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
 
-# Start of element P7
-ChangePinName(P7, pin, pin)
+# Start of element J12
+ChangePinName(J12, 1, 1)
+ChangePinName(J12, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element J10
+ChangePinName(J10, 1, 1)
+ChangePinName(J10, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
 
 # Start of element R8
 ChangePinName(R8, 1, 1)
 ChangePinName(R8, 2, 2)
 
-# Start of element R7
-ChangePinName(R7, 1, 1)
-ChangePinName(R7, 2, 2)
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
 
-# Start of element P4
-ChangePinName(P4, pin, pin)
+# Start of element J9
+ChangePinName(J9, 1, 1)
+ChangePinName(J9, 2, 2)
 
 # Start of element R5
 ChangePinName(R5, 1, 1)
 ChangePinName(R5, 2, 2)
 
+# Start of element J8
+ChangePinName(J8, 1, 1)
+ChangePinName(J8, 2, 2)
+
 # Start of element J7
 ChangePinName(J7, 1, 1)
 ChangePinName(J7, 2, 2)
-
-# Start of element P3
-ChangePinName(P3, pin, pin)
 
 # Start of element J6
 ChangePinName(J6, 1, 1)
@@ -116,8 +105,9 @@ ChangePinName(J6, 2, 2)
 ChangePinName(J5, 1, 1)
 ChangePinName(J5, 2, 2)
 
-# Start of element P1
-ChangePinName(P1, pin, pin)
+# Start of element J4
+ChangePinName(J4, 1, 1)
+ChangePinName(J4, 2, 2)
 
 # Start of element J3
 ChangePinName(J3, 1, 1)
@@ -131,31 +121,45 @@ ChangePinName(J2, 2, 2)
 ChangePinName(J1, 1, 1)
 ChangePinName(J1, 2, 2)
 
-# Start of element Q12
-ChangePinName(Q12, B, B)
-ChangePinName(Q12, E, E)
-ChangePinName(Q12, C, C)
+# Start of element CONN5
+ChangePinName(CONN5, 5, 5)
+ChangePinName(CONN5, 3, 3)
+ChangePinName(CONN5, 1, 1)
+ChangePinName(CONN5, 6, 6)
+ChangePinName(CONN5, 4, 4)
+ChangePinName(CONN5, 2, 2)
 
-# Start of element J12
-ChangePinName(J12, 1, 1)
-ChangePinName(J12, 2, 2)
+# Start of element CONN4
+ChangePinName(CONN4, 5, 5)
+ChangePinName(CONN4, 3, 3)
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 6, 6)
+ChangePinName(CONN4, 4, 4)
+ChangePinName(CONN4, 2, 2)
 
-# Start of element J11
-ChangePinName(J11, 1, 1)
-ChangePinName(J11, 2, 2)
+# Start of element CONN3
+ChangePinName(CONN3, 7, 7)
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 8, 8)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 7, 7)
+ChangePinName(CONN2, 5, 5)
+ChangePinName(CONN2, 3, 3)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 8, 8)
+ChangePinName(CONN2, 6, 6)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
 
 # Start of element CONN1
 ChangePinName(CONN1, 2, 2)
 ChangePinName(CONN1, 1, 1)
-
-# Start of element P12
-ChangePinName(P12, pin, pin)
-
-# Start of element P11
-ChangePinName(P11, pin, pin)
-
-# Start of element P10
-ChangePinName(P10, pin, pin)
 
 # Start of element Q9
 ChangePinName(Q9, B, B)
